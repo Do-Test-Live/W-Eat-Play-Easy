d3beb8784a5017deccf1f56e210062b1ee4d35d3
Mugdho <saha2171515@stud.kuet.ac.bd>
2023-09-13 18:40:56 +0600

    13/09/2023

---
+++ macau.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <title>Play Eat Easy</title>
+    <link href="assets/vendor/Bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="assets/vendor/FontAwesome/css/all.min.css"/>
+    <link href="assets/css/chill_style.css" rel="stylesheet"/>
+</head>
+<body>
+<section class="head">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand d-md-none d-block" href="#"><img src="assets/img/1.png" alt=""
+                                                                    class="img-fluid ls-mobile-logo ls-logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 menu">
+                    <li class="nav-item d-md-block d-none">
+                        <img src="assets/img/1.png" class="img-fluid ls-logo" alt="">
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link"
+                           href="https://playeateasy.com/category/%e5%84%aa%e6%83%a0%e6%b8%9b%e5%83%b9/">優惠減價</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Chill遊日本</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://playeateasy.com/category/%e7%8e%a9%e6%a8%82/">玩樂</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://playeateasy.com/category/%e9%a3%b2%e9%a3%9f/">飲食</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://playeateasy.com/category/%e8%b3%bc%e7%89%a9/">購物</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2 search" type="search" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+
+        </div>
+    </nav>
+</section>
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel"
+     style="margin-top: -95px;">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/img/chill/BANNER.png" class="w-100" alt="..." id="header-image-pc">
+            <img src="assets/img/chill/BANNER.png" class="w-100" alt="..." id="header-image-mobile">
+            <div class="carousel-caption">
+                <div class="row">
+                    <div class="col-lg-12 col-12">
+                        <h1 class="head2">CHILL遊澳門</h1>
+                        <div class="banerpostb mt-2 mt-lg-5">
+                            <button type="button" class="btn btn-lg ls-btn">閱讀更多</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<section class="secondsection">
+    <div class="container">
+        <div class="mt-3 mb-4">
+            <h2 class="ls-title">編輯重點推介</h2>
+        </div>
+        <div class="row mb-4">
+            <div class="col-lg-6 col-12">
+                <img class="img-fluid" src="assets/img/chill/left-img.png">
+            </div>
+            <div class="col-lg-6 col-12 mt-lg-0 mt-5">
+                <img class="img-fluid" src="assets/img/chill/right-img.png">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mdle">
+    <div class="container">
+        <div class="mt-3 mb-4">
+            <h2 class="ls-title">編輯重點推介</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-5">
+                <p class="paragrap">
+                    日本最高戶外展望台！澀谷新地標360度飽覽東京最美夜景
+                </p>
+                <p class="head8">
+                    疫情期間幾年無去日本，澀谷新地標「SHIBUYA
+                    SKY」已經開幕！離地面230米、2500平方米的開放式展望空間，成為日本最高戶外展望台，可無障礙360度飽覽東京最美夜景，地標包括澀谷十字路口、東京晴空塔、東京鐵塔、富士山等都可眺望到！其中外面的扶手電梯都係打卡點之一，一望無際東京夜景超震撼！宜家仲有門票優惠成人$109起！
+                </p>
+                <div class="mt-2 mt-lg-5 buutton">
+                    <a href="https://playeateasy.com/%e6%97%a5%e6%9c%ac%e6%9c%80%e9%ab%98%e6%88%b6%e5%a4%96%e5%b1%95%e6%9c%9b%e5%8f%b0%ef%bc%81%e6%be%80%e8%b0%b7%e6%96%b0%e5%9c%b0%e6%a8%99360%e5%ba%a6%e9%a3%bd%e8%a6%bd%e6%9d%b1%e4%ba%ac%e6%9c%80/"
+                       target="_blank" class="btn btn-lg ls-btn banerpostbb">閱讀更多</a>
+                </div>
+            </div>
+            <div class="col-lg-7 col-md-6 head9 p-3">
+                <div style="border: 5px solid #fff; border-radius: 26px; background: #fff">
+                    <div class="img-border">
+                        <img src="assets/img/chill/SEC-2A.png" class="target-image" alt="..." style="width: 100%">
+                    </div>
+                </div>
+                <div class="row" style="margin-top: -4rem; margin-left: 2rem; margin-right: 2rem;">
+                    <div class="col-4">
+                        <img src="assets/img/chill/SEC-2A.png" class="img-fluid clickable-image" alt=""
+                             style="cursor: pointer;border: 5px solid #ff941a;border-radius: 20px;">
+                    </div>
+                    <div class="col-4">
+                        <img src="assets/img/chill/SEC-2B.png" class="img-fluid clickable-image" alt=""
+                             style="cursor: pointer;border: 5px solid #ff941a;border-radius: 20px;">
+                    </div>
+                    <div class="col-4">
+                        <img src="assets/img/chill/SEC-2C.png" class="img-fluid clickable-image" alt=""
+                             style="cursor: pointer;border: 5px solid #ff941a;border-radius: 20px;">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section style="padding-bottom: 100px;">
+    <div class="container pt-4">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active tabcolor" data-bs-toggle="tab" href="#tab1">玩</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link tabcolor" data-bs-toggle="tab" href="#tab2">買</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link tabcolor" data-bs-toggle="tab" href="#tab3">食</a>
+            </li>
+        </ul>
+
+        <div class="tab-content mt-2">
+            <div class="tab-pane fade show active" id="tab1">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="#"
+                           target="_blank">
+                            <div class="card" style="margin-top: 10px; height: 430px;">
+                                <img src="assets/img/chill/blog1.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h6 class="card-text">日本旅遊｜星野集團改造舊奈良監獄為酒店
+                                        最古老監獄之一！預訂2024年開幕</h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="#"
+                           target="_blank">
+                            <div class="card" style="margin-top: 10px; height: 430px;">
+                                <img src="assets/img/chill/blog2.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h6 class="card-text">日本最高戶外展望台！澀谷新地標360度飽覽東京最美夜景
+                                    </h6>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="#"
+                           target="_blank">
+                            <div class="card" style="margin-top: 10px; height: 430px;">
+                                <img src="assets/img/chill/blig3.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h6 class="card-text">日本旅遊｜福岡地鐵開放信用卡拍卡入閘！$35上限可搭足全日～</h6>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="tab2">
+                <div class="row">
+                </div>
+            </div>
+            <div class="tab-pane fade" id="tab3">
+                <div class="row">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pazel">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 text-start">
+                <h3 class="mt-3 head6" style="color: #ffffff;font-size: 50px;font-weight: 700;">編輯葡國美食之旅</h3>
+            </div>
+        </div>
+    </div>
+    <div class="" style="margin-bottom: -45px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-12 picsec">
+                    <a href="#" target="_blank">
+                        <img src="assets/img/chill/video01.png" class="img-fluid" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-7 col-12 picsec">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <a href="#"
+                                           target="_blank">
+                                            <img src="assets/img/chill/video02.png" class="img-fluid" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-4">
+                                        <a href="#" target="_blank">
+                                            <img src="assets/img/chill/video03.png" class="img-fluid" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-4">
+                                        <a href="#" target="_blank">
+                                            <img src="assets/img/chill/video04.png" class="img-fluid" alt="">
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                                data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                                data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<section class="footer">
+    <div class="container">
+        <div class="Social-media">
+            <a href="https://www.facebook.com/playeateasy/" target="_blank"><font color="#007cc4"><i
+                            class="fab fa-facebook"></i></font></a>
+            <a href="https://www.instagram/com/playeateay" target="_blank"><font color="#007cc4"><i
+                            class="fab fa-instagram"></i></font></a>
+            <a href="https://www.youtube.com/channel/UCAs4cR685Txuk3zSvhMsnEA" target="_blank"><font color="#007cc4"><i
+                            class="fab fa-youtube"></i></font></a>
+        </div>
+        <div class="row text-center mt-3">
+            <div class="col-md-12 pb-md-0 pb-3">
+                <h6 class="footercontent">Copyright © 2023 Play Eat Easy. All rights reserved. Powered by <a
+                            href="https://ngt.hk/" class="text-decoration-none text-white">NGT-Tech</a>.</h6>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<script src="assets/vendor/Bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/jQuery/jquery-3.6.4.min.js"></script>
+<script src="assets/vendor/OwlCarousel/js/owl.carousel.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+<script>
+    let clickableImages = document.querySelectorAll(".clickable-image");
+
+    clickableImages.forEach(function (image) {
+        image.addEventListener("click", function () {
+            changeImageSrc(image);
+        });
+    });
+
+    function changeImageSrc(clickedImage) {
+        // Get the src of the clicked image
+        let src = clickedImage.src;
+
+        // Find the target image by class
+        let targetImage = document.querySelector(".target-image");
+
+        // Set the src of the target image to the src of the clicked image
+        targetImage.src = src;
+    }
+</script>
+
+</body>
+</html>
